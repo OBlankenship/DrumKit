fc7d595160b7df97f40c6384ab387899c72d181e
Oliver Blankenship <oliverblankenship@gmail.com>
2023-01-14 20:29:34 -0600

    Implement removal of animation
    Implemented listeners that will remove all animations upon
    completion.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+// Play sound, and add animation to the element
 window.addEventListener('keydown', function(e) {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
-    key.classList.add("playing")
+    key.classList.add("playing");
 })
+
+// Add listener to remove animation
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', () => key.classList.remove("playing")));
